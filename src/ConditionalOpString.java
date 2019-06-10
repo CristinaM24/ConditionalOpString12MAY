ae2ce250080a15f28edf40cf5f2696ef990e97e3
cristinam <mateescu.cristina24@yahoo.ro>
2019-06-10 16:15:35 +0300

    initial commit

---
+++ src/ConditionalOpString.java
@@ -0,0 +1,10 @@
+public class ConditionalOpString {
+    public static void main(String[] args) {
+        int a=50;
+        int b=10;
+        String result=a%b==0 ? "ok": "not ok";
+        System.out.println(result);
+
+
+    }
+}
